b4e790bd3f0d7d964264d596170caefdd1c29690
coderfoodie <lsomodio@gmail.com>
2020-12-25 18:03:18 +0800

    Simple string concatenation and github push for the first time. DailyCodeChallenge Day1

---
+++ string-concatenate.py
@@ -0,0 +1,14 @@
+# String concatenation
+# youtuber = 'Luvies'
+
+# print('Subscribe to ' + youtuber)
+# print('Subscribe to {}'.format(youtuber))
+# print(f'Subscribe to {youtuber}')
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+famous_person = input('Famous Person: ')
+
+madlibs = f'Computer programming is so {adj}! It makes me so excited all the time because \n I love to {verb1}, Stay hydrated and {verb2} like you are {famous_person}'
+
+print(madlibs)  
