1c02f0900613310c82cdcf9c4b2e1823a25ad95d
coderfoodie <lsomodio@gmail.com>
2021-01-02 16:49:07 +0800

    Created a stacks and queues

---
+++ stackandqueue.py
@@ -0,0 +1,27 @@
+stack = []
+queue = []
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+stack.append(5)
+
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+queue.append(5)
+
+print("Stack: ", stack)
+
+stack.pop()
+print("Stack: ", stack)
+
+
+print("Queue: ", queue)
+
+queue.pop()
+print("Queue: ", queue)
+
+
