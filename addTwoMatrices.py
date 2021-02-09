99e3c5c88a96249a7cfb789e9277c1964f85b1c7
Luz Somodio <coderfoodie@users.noreply.github.com>
2021-02-09 08:44:19 +0800

    update addTwoMatrices.py

--- addTwoMatrices.py
+++ addTwoMatrices.py
@@ -1,4 +1,3 @@
-# using for loop
 x = [[1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]]
@@ -7,12 +6,26 @@ y = [[9, 8, 7],
     [6, 5, 4],
     [3, 2, 1]]
 
-result = [[0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
+# using for loop
 
-for i in range(len(x)):
-    for j in range(len(x[0])):
-        result[i][j] = x[i][j] + y[i][j]
-for r in result:
-    print(r)
+# result = [[0, 0, 0],
+#         [0, 0, 0],
+#         [0, 0, 0]]
+
+# for i in range(len(x)):
+#     for j in range(len(x[0])):
+#         result[i][j] = x[i][j] + y[i][j]
+# for r in result:
+#     print(r)
+
+#using nested list
+
+# result = [[x[i][j] + y[i][j] for j in range(len(x[0]))] for i in range(len(x))]
+
+# for r in result:
+#     print(r)
+
+# using zip() and sum
+
+result = [map(sum, zip(*t)) for t in zip(x, y)]
+print(result)
